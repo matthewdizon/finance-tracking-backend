518eb116b116719c364c50c9c73afd20643641db
Matthew Dizon <dominic.dizon@obf.ateneo.edu>
2022-07-24 12:43:36 +0800

    added mongoose and connected to database

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const transactionRoutes = require("./routes/transactions");
 
 const app = express();
@@ -14,6 +15,13 @@ app.use((req, res, next) => {
 
 app.use("/api/transactions", transactionRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log("listening on port", process.env.PORT);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
