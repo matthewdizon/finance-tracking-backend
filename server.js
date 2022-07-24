acece4f63db46b038892fa2a85340c3ab1b0f6fd
Matthew Dizon <dominic.dizon@obf.ateneo.edu>
2022-07-24 11:49:43 +0800

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.get("/", (req, res) => {
+  //   console.log(req);
+  res.json({ mssg: "Welcome to the app" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("listening on port", process.env.PORT);
+});
