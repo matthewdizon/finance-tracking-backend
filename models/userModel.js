06ffea4691fc4eac33a0d030d994d0e97edf1bc0
Matthew Dizon <dominic.dizon@obf.ateneo.edu>
2022-08-04 00:49:03 +0800

    feat: email and password validator

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -20,6 +21,18 @@ const userSchema = new Schema(
 
 // static signup method
 userSchema.statics.signup = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error("Email is invalid");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password is not strong enough");
+  }
+
   const exists = await this.findOne({ email });
 
   if (exists) {
